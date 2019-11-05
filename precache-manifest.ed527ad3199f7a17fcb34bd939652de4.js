7d79dca7c6ab1ed9e9cfcae52548a6ed312dc761
marvin lara <laramarvin105@gmail.com>
2019-11-05 18:30:02 -0500

    Updates

---
+++ precache-manifest.ed527ad3199f7a17fcb34bd939652de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6075049b1fb12de4c0a79f62a1e054a3",
+    "url": "/Guessgame_React/index.html"
+  },
+  {
+    "revision": "d0fabe311599918f2953",
+    "url": "/Guessgame_React/static/css/main.9a8442bb.chunk.css"
+  },
+  {
+    "revision": "761977ef033070f989bd",
+    "url": "/Guessgame_React/static/js/2.0b8134d6.chunk.js"
+  },
+  {
+    "revision": "d0fabe311599918f2953",
+    "url": "/Guessgame_React/static/js/main.d7ea34a6.chunk.js"
+  },
+  {
+    "revision": "1d64fbfc4216317dc82e",
+    "url": "/Guessgame_React/static/js/runtime-main.b6896914.js"
+  }
+]);
